d1689505b9cdcbe025ed52f2ed91ba07b86b62c1
chuspita <kosecki99@gmail.com>
2023-01-30 18:42:24 +0100

    sm6150-common: Rename kernel name

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,7 +90,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld LLVM=1 LLVM_IAS=1
 TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_SUPPORTS_LLVM_TOOLS := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/aghasni
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CLANG_VERSION := azure
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-azure
 
